c2c18ca15840eb0960dcaef7b3cd1c6ffd6c4131
iMrEaLlYaNoOb12 <100996209+iMrEaLlYaNoOb12@users.noreply.github.com>
2023-08-19 15:33:09 +0530

    Add files via upload

---
+++ java2-prashanth-s-cdc/src/edu/disease/asn2/InfectiousDisease.java
@@ -0,0 +1,11 @@
+package edu.disease.asn2;
+
+public class InfectiousDisease extends Disease {
+
+	@Override
+	public String[] getExamples() {
+		String infectiousDisease[] = { "Chickenpox", "Diphtheria", "Influenza", "Influenza" };
+		return infectiousDisease;
+	}
+
+}
---
+++ java2-prashanth-s-cdc/src/edu/disease/asn2/NonInfectiousDisease.java
@@ -0,0 +1,11 @@
+package edu.disease.asn2;
+
+public class NonInfectiousDisease extends Disease {
+
+	@Override
+	public String[] getExamples() {
+		String nonInfectiousDisease[] = { "Diabetes", "Cancers", "Arthiritis", "Asthma" };
+		return nonInfectiousDisease;
+	}
+
+}
